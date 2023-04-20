8ff152ab713a9a76d769006dd2594e046ef74d1b
Abdullah Nafees <abdullahnafees23@gmail.com>
2023-04-20 16:04:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordOne = ""
+let passwordTwo = ""
+let passwordLength = 15
+let fieldOne = document.getElementById("field-one");
+let fieldTwo = document.getElementById("field-two");
+
+function getRandomChar() {
+    let randomNumber = Math.floor(Math.random()*characters.length) 
+    return characters[randomNumber]
+}
+function getRandomPassword() {
+    let randomPassword = ""
+    for (let i = 0; i < passwordLength; i++) {
+        randomPassword += getRandomChar()         
+    }
+    return randomPassword
+}
+function getPasswords() {
+    passwordOne = getRandomPassword()
+    passwordTwo = getRandomPassword()
+    fieldOne.textContent = passwordOne
+    fieldTwo.textContent = passwordTwo
+}
+
+
+
